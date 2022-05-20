95360d5c74dbe0b8b121ab96503e9df266542ed4
serhiisergio <dmitriievserguei@gmail.com>
2022-05-20 19:34:37 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f24d4f1f-c963-4650-b224-575701b5bc6e';
+export const MODAL_CLOSE_SEC = 2.5;
